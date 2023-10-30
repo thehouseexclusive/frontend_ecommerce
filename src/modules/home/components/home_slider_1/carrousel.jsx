0eab51f3e95211fe474abd1aa59cdd77bbaee7e8
Miguel Páez <mpaez@ikatechsolutions.com>
2023-10-29 21:55:43 -0500

    Update changes home slider 1

--- src/modules/home/components/home_slider_1/carrousel.jsx
+++ src/modules/home/components/home_slider_1/carrousel.jsx
@@ -36,10 +36,14 @@ function Carrusel() {
   return (
     <div className="carousel" style={{ backgroundImage: `url(${imagenes[currentImageIndex]})` }}>
       <button onClick={goToPreviousSlide}  className="button_carousel ml-2 bottom-5 right-5 w-12 h-12 rounded-full bg-white shadow-md transition-transform hover:scale-110 hover:brightness-90">
-        <img src="/public/icons/caret-left-fill.svg" alt="imagen icono carrusel" className='icon-carousel'/>
+        <svg className='icon-carousel' xmlns="http://www.w3.org/2000/svg"  fill="currentColor"  viewBox="0 0 16 16">
+          <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+        </svg>
       </button>
       <button onClick={goToNextSlide} className="button_carousel mr-2 bottom-5 right-5 w-12 h-12 rounded-full bg-white shadow-md transition-transform hover:scale-110 hover:brightness-90">
-        <img src="/public/icons/caret-right-fill.svg" alt="imagen icono carrusel" className='icon-carousel'/>
+      <svg className='icon-carousel' xmlns="http://www.w3.org/2000/svg"  fill="currentColor"  viewBox="0 0 16 16">
+        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+      </svg>
       </button>
     </div>
     
